c6d302a973a617f25347486cf07ad163012060a6
hectormc <hector.carrillo.1423@gmail.com>
2023-04-19 17:33:24 -0400

    it's fine II

--- app/controllers/lead_controller.rb
+++ app/controllers/lead_controller.rb
@@ -1,2 +1,27 @@
 class LeadController < ApplicationController
+  def new
+    @lead = Lead.new
+  end
+
+  def create
+    @lead = Lead.new(lead_params)
+
+    if @lead.save
+      something
+    else
+
+    end
+  end
+
+  private
+  def lead_params
+    params.require(:lead).permit(:e_mail,
+                                 :phone_number,
+                                 :current_city,
+                                 :move_in_city,
+                                 :price_range_low,
+                                 :prince_range_high,
+                                 :salary,
+                                 :credit_score)
+  end
 end
